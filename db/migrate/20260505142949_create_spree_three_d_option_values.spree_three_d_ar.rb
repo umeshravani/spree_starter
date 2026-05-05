30aaf5e9e87494a735ba1b3866fe75cbe115df21
Umesh Ravani <umeshravani98@gmail.com>
2026-05-05 14:45:32 +0000

    Fix class NameError in option_values migration

--- db/migrate/20260505142949_create_spree_three_d_option_values.spree_three_d_ar.rb
+++ db/migrate/20260505142949_create_spree_three_d_option_values.spree_three_d_ar.rb
@@ -1,16 +1,10 @@
-# This migration comes from spree_three_d_ar (originally 20260504124925)
-class CreateSpreeThreeDMaterialOptionValues < ActiveRecord::Migration[7.0]
+class CreateSpreeThreeDOptionValues < ActiveRecord::Migration[8.0]
   def change
-    create_table :spree_three_d_material_option_values, if_not_exists: true do |t|
-      t.references :three_d_material, 
-                   null: false, 
-                   foreign_key: { to_table: :spree_three_d_materials }, 
-                   index: { name: 'idx_spree_3d_mat_opt_val_on_mat_id' }
-                   
-      t.references :three_d_option_value, 
-                   null: false, 
-                   foreign_key: { to_table: :spree_three_d_option_values }, 
-                   index: { name: 'idx_spree_3d_mat_opt_val_on_val_id' }
+    create_table :spree_three_d_option_values, if_not_exists: true do |t|
+      t.references :three_d_option_type, null: false, foreign_key: { to_table: :spree_three_d_option_types }
+      t.string :name
+      t.string :presentation
+      t.string :color_code
 
       t.timestamps
     end
