9b99cba911892788a136f1f9f25351fc3ddf963e
Umesh Ravani <umeshravani98@gmail.com>
2026-06-11 16:51:31 +0000

    Fix tailwind config and restore v4

---
+++ config/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.erb',
+    './app/views/spree/**/*.erb',
+    './app/views/devise/**/*.erb'
+  ]
+}
