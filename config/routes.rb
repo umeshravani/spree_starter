2b0b72037cdf6b59bfeadbde7fb0f7d0ec97cbdb
Umesh Ravani <umeshravani98@gmail.com>
2026-06-12 11:09:25 +0000

    Changed Routing from Storefront to Admin

--- config/routes.rb
+++ config/routes.rb
@@ -49,5 +49,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "spree/home#index"
+  #root "spree/home#index"
+  root to: redirect('/admin')
 end
